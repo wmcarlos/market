4c6ccf20a2bdda75676529473ba9f1e83310fa10
Carlos Vargas <info-analista7@iancarina.com.ve>
2016-12-08 12:30:44 -0400

    upload 100% from database structure

--- db/market.sql
+++ db/market.sql
@@ -126,5 +126,76 @@ create table mk_pubication(
 	constraint fk_market_id foreign key (market_id) references mk_market (market_id) on update cascade on delete restrict
 )engine = InnoDB;
 
+create table mk_comment(
+	comment_id int auto_increment not null,
+	publication_id int not null,
+	user_id int not null,
+	created timestamp not null default current_timestamp,
+	updated timestamp null,
+	content varchar(255) not null,
+	isactive char(1) not null default 'Y',
+	constraint pk_commnet_id primary key (comment_id),
+	constraint fk_publication_id foreign key (publication_id) references mk_publication (publication_id) on update cascade on delete restrict,
+	constraint fk_user_id01 foreign key (user_id) references mk_user (user_id) on update cascade on delete restrict
+ )engine = InnoDB;
 
+create table mk_social(
+	social_id int auto_increment not null,
+	market_id int not null,
+	created timestamp not null default current_timestamp,
+	updated timestamp null,
+	url text,
+	isactive char(1) not null default 'Y',
+	constraint pk_social_id primary key (social_id),
+	constraint fk_market_id foreign key (market_id) references mk_market (market_id) on update cascade on delete restrict
+)engine = InnoDB;
 
+create table mk_follower(
+	follower_id int auto_increment not null,
+	user_id int not null.
+	market_id int not null,
+	created timestamp not null default current_timestamp,
+	updated timestamp null,
+	isactive char(1) not null default 'Y',
+	constraint pk_follower_id primary key (followe_id),
+	constraint fk_user_id02 foreign key (user_id) references mk_user (user_id) on update cascade on delete restrict,
+	constraint fk_market_id01 foreign key (market_id) references mk_market (market_id) on update cascade on delete restrict
+)engine = InnoDB;
+
+create table mk_notification(
+	notification_id int auto_increment not null,
+	user_id int not null,
+	created timestamp not null default current_timestamp,
+	updated timestamp null,
+	content varchar(255) not null,
+	status char(2) not null default 'NR',
+	isactive char(1) not null default 'Y',
+	constraint pk_notification_id primary key (notification_id),
+	constraint fk_user_id03 foreign key (user_id) references mk_user (user_id) on update cascade on delete restrict
+)engine = InnoDB;
+
+create table mk_media(
+	media_id int auto_increment not null,
+	user_id int not null,
+	media_format_id int not null,
+	created timestamp not null default current_timestamp,
+	updated timestamp null,
+	name varchar(30) not null,
+	description varchar(250) null,
+	url varchar(100) not null,
+	size number not null,
+	isactive char(1) not null default 'Y',
+	constraint pk_media_id primary key (media_id),
+	constraint fk_user_id04 foreign key (user_id) references mk_user (user_id) on update cascade on delete restrict,
+	constraint fk_media_format_id foreign key (media_format_id) references mk_value (value_id) on update cascade on delete restrict
+)engine = InnoDB;
+
+create table mk_media_assigned(
+	media_assigned_id int auto_increment not null,
+	media_id int not null,
+	publication_id int not null,
+	created timestamp not null default current_timestamp,
+	constraint pk_media:assigned_id primary key (media_assigned_id),
+	constraint fk_media_id foreign key (media_id) references mk_media (media_id) on update cascade on delete restrict,
+	constraint fk_publication_id001 foreign key (publication_id) references mk_publication (publication_id) on update cascade on delete restrict
+)engine = InnoDB;
